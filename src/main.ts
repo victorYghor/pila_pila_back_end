fc500a00b29a00d875e0e74997cacc14bfb07a22
victor-yghor <devictoryghor@gmail.com>
2026-04-25 04:08:58 -0300

    solve the problem with prisma

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
